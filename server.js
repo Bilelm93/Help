99692d42a8e947b14fa3218f219ba01ce66d14c0
bilel <bilel.m92@gmail.com>
2021-04-28 22:44:52 +0100

    deploy

--- server.js
+++ server.js
@@ -14,7 +14,14 @@ app.use('/api/user', require('./routers/user'));
 app.use('/api/subvention', require('./routers/subvention'));
 app.use('/api/type',  require('./routers/type'));
 app.use('/api/msg' , require('./routers/msg'))
+if (process.env.NODE_ENV === "production") {
 
+    app.use(express.static("client/build"))
+    
+    app.get("/*" , (req, res)=>{
+        res.sendFile(path.resolve(__dirname, "../client" , "build" , "index.html"))
+    })
+    }
 
 const PORT = process.env.PORT || 5000;
 
